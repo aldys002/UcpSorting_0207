50314c34a4a673a212df3b3a1d22c41b53a7c47d
aldys002 <igidiaaldys@gmail.com>
2024-03-21 13:50:47 +0700

    Add project files.

---
+++ UCP 1.cpp
@@ -0,0 +1,24 @@
+// UCP 1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+1. insertion sort pengurutan dg
+
+#include <iostream>
+using namespace std;
+int n, int arr[70];
+
+void input() {
+    while (true) {
+        cout << "Masukkan banyaknya elemen pada Array : ";
+        cin >> n;
+        if (n <= 70)
+            break
+
+    }
+}
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+
